8c4c1c7362e2052411caa9738cb30b8da1a72037
NandorDobo <147068167+NandorDobo@users.noreply.github.com>
2024-03-05 17:39:05 +0000

    Add files via upload

--- Project/firebase/store.py
+++ Project/firebase/store.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import datetime
 from datetime import datetime
 import serial
 from serial.tools.list_ports import comports
@@ -40,7 +41,7 @@ ref = db.reference("/user/preferences")
 ref.child("study_time").set(20)
 ref.child("sport_time").set(20)
 ref = db.reference("/user/times")
-ref.child("study_time").set(40)
+ref.child("study_time").set(0)
 ref.child("sport_time").set(40)
 #Read
 # ref = db.reference('/user/resttime')
@@ -70,21 +71,20 @@ def today_times(remaining,date):
     ref = db.reference("/user/days/"+date+"/remaining_times")
     ref.child("sport_time").set(sport_time)
     
-    
-    ref = db.reference('/user/times')
     rest_time = remaining
+    ref = db.reference("/user/days/"+date+"/remaining_times")
+    ref.child("rest_time").set(rest_time)
 
-    return rest_time
+    
 
-
-
-def start():
+def start(date):
     name = input("What is your username?")
     print("What do you want to do first?")
     prev = input("study,phisical activity,rest")
     working_values = {}
     working_values["name"] = name
-    working_values["first"] = prev
+    ref = db.reference("/user/days/"+date+"/")
+    ref.child("start").set(prev)
     working_values["prev"] = prev
     return working_values
 
@@ -101,7 +101,7 @@ def send(working_values):
     ser.write((str(task_time)+",").encode("utf-8"))
     return(task_time)
 
-def validation(task_time):
+def validation():
     while True:
         rec = ser.readline().decode('utf-8').strip()
         if (rec != ""):
@@ -121,22 +121,91 @@ def update(working_values,date,task_time):
     
     ref = db.reference("/user/days/"+date+"/remaining_times/"+working_values["prev"]+ "_time")
     time = ref.get() - (task_time - int(rec[0]))
+    
     print(time)
     ref = db.reference("/user/days/"+date+"/remaining_times/")
     ref.child(working_values["prev"] + "_time").set(time)
+    
+    ref = db.reference("/user/days/"+date+"/fails/")
+    fail = ref.get()
+    ref = db.reference("/user/days/"+date+"/")
+    ref.child("fails").set(fail + int(rec[1]))
+    
+def recommend(working_values,date):
+    if(working_values["prev"] == "sport"):
+        recommend = "rest"
+    elif(working_values["prev"] == "study"):
+        ref = db.reference("/user/days/"+date+"/remaining_times/"+"sport_time")
+        if(ref.get() != 0):
+            recommend = "sport"
+        else:
+            recommend = "rest"
+    else:
+        ref = db.reference("/user/days/"+date+"/remaining_times/"+"study_time")
+        study_time = ref.get()
+        ref = db.reference("/user/days/"+date+"/remaining_times/"+"sport_time")
+        sport_time = ref.get()
+        if(study_time != 0):
+            recommend = "study"
+        elif(sport_time != 0):
+            recommend = "sport"
+        else:
+            recommend = "rest"
+    return recommend
 
+def weekday(date):
+    date_parts = date.split("-")
+    year = int(date_parts[0])
+    month = int(date_parts[1])
+    day = int(date_parts[2])
+    given_date = datetime(year, month, day)
+    day_of_week = given_date.weekday()
+
+    
+    if(day_of_week == 0):
+        day_of_week = "monday"
+    elif(day_of_week == 1):
+        day_of_week = "tuesday"
+    elif(day_of_week == 2):
+        day_of_week = "wednesday"
+    elif(day_of_week == 3):
+        day_of_week = "thursday"
+    elif(day_of_week == 4):
+        day_of_week = "friday"
+    elif(day_of_week == 5):
+        day_of_week = "saturday"
+    else:
+        day_of_week = "sunday"
+    ref = db.reference("/user/days/"+date+"/")
+    ref.child("day_of_week").set(day_of_week)
 
 def main():
     date = datetime.today().date().isoformat()
     remaining = rtime()
     print(remaining)
+    ref = db.reference("/user/days/"+date+"/")
+    ref.child("fails").set(0)
+    
+    weekday(date)
     
     rest_time = today_times(remaining,date)
     print(rest_time)
     
-    working_values = start()
+    working_values = start(date)
     task_time = send(working_values)
     update(working_values,date,task_time)
+    while True:
+        recommended = recommend(working_values,date)
+        print("What do you want to do next?, I recommend "+recommended)    
+        prev = input()
+        working_values["prev"] = str(prev)
+        task_time = send(working_values)
+        update(working_values,date,task_time)
+
+        if(remaining == 30):
+            print("It is time to go to bed get ready for it. Good Night")
+            break
+    
 main()
 
 # def end()
